b812dc867ee4e0661ce4f923b2e4256ab4c60db2
Mike Lopez <lopes92290@gmail.com>
2026-04-15 22:02:20 -0400

    cleanup day 2

--- day2_part2.py
+++ day2_part2.py
@@ -8,44 +8,48 @@ for r in lines[0].strip().split(','):
     rr = r.split('-')
     ranges.append((int(rr[0]), int(rr[1])))
 
-def mcustom(inc, start):
+def pattern_generator(inc, start):
     if start == 0:
         return 1
     
-    return mcustom(inc, start - inc) + 10**start 
+    return pattern_generator(inc, start - inc) + 10**start 
 
 seen = set()
 total = 0
 for r in ranges:
-    c = r[0]
-    while c <= r[1]:
-        if c < 10:
-            c += 1
+    cur_num = r[0]
+    while cur_num <= r[1]:
+        if cur_num < 10:
+            cur_num += 1
             continue
 
-        l = math.floor(math.log10(c)) + 1
+        num_len = math.floor(math.log10(cur_num)) + 1
 
-        s = str(c)
-        if int(s[0]) * ((10**l - 1) // 9) == c:
-            if c not in seen:
-                seen.add(c)
-                total += c
+        first_digit = int(str(cur_num)[0])
+        #see if cur_num is a single digit repeating number pattern
+        if first_digit * ((10**num_len - 1) // 9) == cur_num: # ex 5 * 1111 = 5555
+            if cur_num not in seen:
+                seen.add(cur_num)
+                total += cur_num
         else:
-            stop = l // 2
-            cur = 1
-            while cur != stop:
-                if l % (cur + 1) != 0:
-                    cur += 1
+            stop = num_len // 2 #if this is a repeating pattern we know the pattern must repeat before the mid of the number
+            cur_i = 1
+            while cur_i != stop:
+                num_repeating_digits = cur_i + 1
+                if num_len % num_repeating_digits != 0: # we only care about possible patterns ex a 10 digit number can't have a repeating pattern of 3
+                    cur_i += 1
                     continue
 
-                m = mcustom(cur + 1, l - (cur + 1))
+                #generate a number of a 1s and 0s that when cur_num is divided would equal the pattern of cur_num
+                #ex: 121212 // 10101 = 12 so 121212 % 10101 = 0
+                pattern = pattern_generator(num_repeating_digits, num_len - num_repeating_digits)
 
-                if c % m == 0:
-                    if c not in seen:
-                        seen.add(c)
-                        total += c             
+                if cur_num % pattern == 0:
+                    if cur_num not in seen:
+                        seen.add(cur_num)
+                        total += cur_num             
 
-                cur += 1
-        c += 1
+                cur_i += 1
+        cur_num += 1
     
 print(total)
